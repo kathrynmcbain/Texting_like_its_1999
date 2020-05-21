1d6b1d4202d06f652794e2f958de93429ffcddc7
Kathryn <kathryn@Johns-iMac.home>
2020-05-21 01:18:07 +0100

    basic JS tested

---
+++ JS/Texting.js
@@ -0,0 +1,42 @@
+const abc = document.getElementById("1");
+const ghi = document.getElementById("4");
+const output = document.getElementById('screen');
+
+let buttonTimeout = null;
+let incrementer = 0;
+
+const alphaA = ['a','b','c','a','b','c'];
+const alphaG=['g','h','i','g','h','i'];
+
+var text='';
+var newLetter='';
+
+abc.addEventListener('click', () => {
+    if (!buttonTimeout) {
+        buttonTimeout = setTimeout(() => {
+            newLetter=alphaA[incrementer-1];
+            console.log('finished: '+newLetter);
+            text=text+newLetter;
+            console.log('total text: '+text);
+            incrementer = 0;
+            buttonTimeout = null;
+           // abc.removeAttribute('disabled');
+        }, 1000);
+    }
+    output.innerHTML = alphaA[incrementer];
+    incrementer += 1;
+});
+ghi.addEventListener('click', () => {
+    if (!buttonTimeout) {
+        buttonTimeout = setTimeout(() => {
+            newLetter=alphaG[incrementer-1];
+            text=text+newLetter;
+            console.log('total text: '+text);
+            incrementer = 0;
+            buttonTimeout = null;
+            // abc.removeAttribute('disabled');
+        }, 1000);
+    }
+    output.innerHTML = alphaG[incrementer];
+    incrementer += 1;
+});
