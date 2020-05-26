f2fe1fde161fe6d38536b8cee303f1849d268e23
Kathryn <kathryn@Johns-iMac.home>
2020-05-26 21:46:05 +0100

    Implemented a single function for getting input

--- JS/Texting.js
+++ JS/Texting.js
@@ -1,42 +1,20 @@
-const abc = document.getElementById("2");
-const ghi = document.getElementById("4");
 const output = document.getElementById('screen');
 
 let buttonTimeout = null;
 let incrementer = 0;
 
-const alphaA = ['a','b','c','a','b','c'];
-const alphaG=['g','h','i','g','h','i'];
+let text='';
+let newLetter='';
 
-var text='';
-var newLetter='';
-
-abc.addEventListener('click', () => {
+function buttonPressed(letterArray){
     if (!buttonTimeout) {
         buttonTimeout = setTimeout(() => {
-            newLetter=alphaA[incrementer-1];
-            console.log('finished: '+newLetter);
+            newLetter=letterArray[incrementer-1];
             text=text+newLetter;
-            console.log('total text: '+text);
             incrementer = 0;
             buttonTimeout = null;
-           // abc.removeAttribute('disabled');
         }, 1000);
     }
-    output.innerHTML = alphaA[incrementer];
+    output.innerHTML = text+letterArray[incrementer];
     incrementer += 1;
-});
-ghi.addEventListener('click', () => {
-    if (!buttonTimeout) {
-        buttonTimeout = setTimeout(() => {
-            newLetter=alphaG[incrementer-1];
-            text=text+newLetter;
-            console.log('total text: '+text);
-            incrementer = 0;
-            buttonTimeout = null;
-            // abc.removeAttribute('disabled');
-        }, 1000);
-    }
-    output.innerHTML = alphaG[incrementer];
-    incrementer += 1;
-});
+}
